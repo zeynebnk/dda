24821385c2a71a130bf481a331fbbc749740490f
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-05-05 01:48:21 -0700

    Update lm_eval.py

--- gen_eval/lm_eval.py
+++ gen_eval/lm_eval.py
@@ -8,11 +8,11 @@ def evaluate_model(model_path, peft_path=None, task=None):
 
     if peft_path:
         run_command = f"cd lm-evaluation-harness/lm_eval/ && python __main__.py --model hf \
-        --model_args pretrained={model_path},load_in_4bit=True,peft={peft_path} \
+        --model_args pretrained={model_path},load_in_4bit=True,peft=../../{peft_path} \
         --tasks {task}"
     else:
         run_command = f"cd lm-evaluation-harness/lm_eval/ && python __main__.py --model hf \
-        --model_args pretrained=./{model_path} \
+        --model_args pretrained={model_path} \
         --tasks {task}"
 
     subprocess.run(run_command, shell=True)
